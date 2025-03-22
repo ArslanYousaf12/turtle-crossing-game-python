d37ba821caf829d2458bcbe7eb7b8fc1e544df8b
Arslan Yousaf <devtechofficial90@gmail.com>
2025-03-22 12:07:41 +0500

    player class functionality added as well as car

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,25 @@
+from turtle import Turtle
+import random
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        self.penup()
+        self.color(random.choice(COLORS))
+        self.shapesize(stretch_wid=1, stretch_len= 2)
+        self.start_position()
+        
+    
+    def start_position(self):
+        random_y = random.randint(-290, 290)
+        self.goto(300, random_y)
+        # self.goto(0,0)
+    
+    def move(self):
+        self.setheading(180)
+        self.forward(STARTING_MOVE_DISTANCE)
